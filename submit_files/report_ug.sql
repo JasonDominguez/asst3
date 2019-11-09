0ebfb013392e5f4f3088bcdbd07ba642ea8f6fd7
hbrow <hbrow006@odu.edu>
2019-11-09 13:19:56 -0500

    trying to follow example: insert procedure

--- submit_files/report_ug.sql
+++ submit_files/report_ug.sql
@@ -1,5 +1,23 @@
-CREATE OR REPLACE PROCEDURE ins_dept_summary AS
+CREATE OR REPLACE PROCEDURE ins_dept_summary(
+       v_dname dept_summary.dname%TYPE, 
+       v_dnumber dept_summary.dnumber%TYPE, 
+       v_emp_type dept_summary.emp_type%TYPE, 
+       v_proj_type dept_summary.proj_type%TYPE, 
+       v_num_emps dept_summary.num_emps%TYPE, 
+       v_hours dept_summary.hours%TYPE, 
+       v_cost dept_summary.cost%TYPE, 
+       v_user_name dept_summary.user_name%TYPE, 
+       v_insert_number dept_summary.insert_number%TYPE)
 
-    CURSOR nemps IS
-    select count(*) num_emps
-    from department
+BEGIN
+    cs450.ins_dept_summary(
+       v_dname ,
+       v_dnumber ,
+       v_emp_type ,
+       v_proj_type ,
+       v_num_emps ,
+       v_hours ,
+       v_cost ,
+       'STUDEN_A',
+       v_insert_number);
+END
