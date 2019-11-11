d2655d5d82c01cfbac5066a62a8e3fe4c6e06998
hbrow <hbrow006@odu.edu>
2019-11-10 21:51:46 -0500

    big fixes

--- submit_files/report_ug.sql
+++ submit_files/report_ug.sql
@@ -1,13 +1,13 @@
 CREATE OR REPLACE PROCEDURE report_ug(
-    v_dname dept_summary.dname%TYPE, 
-    v_dnumber dept_summary.dnumber%TYPE, 
-    v_emp_type dept_summary.emp_type%TYPE, 
-    v_proj_type dept_summary.proj_type%TYPE, 
-    v_num_emps dept_summary.num_emps%TYPE, 
-    v_hours dept_summary.hours%TYPE, 
-    v_cost dept_summary.cost%TYPE, 
-    v_user_name dept_summary.user_name%TYPE, 
-    v_insert_number dept_summary.insert_number%TYPE) AS
+    v_dname report_ug.dname%TYPE, 
+    v_dnumber report_ug.dnumber%TYPE, 
+    v_emp_type report_ug.emp_type%TYPE, 
+    v_proj_type report_ug.proj_type%TYPE, 
+    v_num_emps report_ug.num_emps%TYPE, 
+    v_hours report_ug.hours%TYPE, 
+    v_cost report_ug.cost%TYPE, 
+    v_user_name report_ug.user_name%TYPE, 
+    v_insert_number dreport_ug.insert_number%TYPE) AS
 
     CURSOR dname IS
     select distinct dname
