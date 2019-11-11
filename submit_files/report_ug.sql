3439132cdc97f6e6ba6acc8f058c7a407b086e4a
hbrow <hbrow006@odu.edu>
2019-11-10 22:17:35 -0500

    big fixes

--- submit_files/report_ug.sql
+++ submit_files/report_ug.sql
@@ -1,28 +1,27 @@
-CREATE OR REPLACE PROCEDURE report_ug AS
-Declare 
-    v_dname dept_summary.dname%TYPE, 
-    v_dnumber dept_summary.dnumber%TYPE, 
-    v_emp_type dept_summary.emp_type%TYPE, 
-    v_proj_type dept_summary.proj_type%TYPE, 
-    v_num_emps dept_summary.num_emps%TYPE, 
-    v_hours dept_summary.hours%TYPE, 
-    v_cost dept_summary.cost%TYPE, 
-    v_user_name dept_summary.user_name%TYPE, 
-    v_insert_number dept_summary.insert_number%TYPE;
+CREATE OR REPLACE PROCEDURE report_ug AS 
 
     CURSOR dname IS
     select distinct dname
     from department;
+    
+    v_dname dept_summary.dname%TYPE;
+
 
     CURSOR dnumber IS
     select distinct dnumber
     from department;
-    
+
+    v_dnumber dept_summary.dnumber%TYPE;
+
+
     CURSOR num_emps IS
     select count(distinct ssn) num_emps
     from employee,department
     where dno=dnumber and
-          dname = v_dname.dname;      
+          dname = v_dname.dname;
+
+    v_num_emps dept_summary.num_emps%TYPE;
+
 
     CURSOR emp_totals IS
     select nvl(sum(hours),0) tot_hours, nvl(sum(hours*salary/2000),0) tot_cost
@@ -32,20 +31,39 @@ Declare
     where pnumber=pno and
           dname = v_dname.dname;
 
+    v_emp_totals dept_summary.emp_totals%TYPE;
+
     insert_number  NUMBER := 0;
         
 
 BEGIN
-    
-    cs450.ins_dept_summary(
-    v_dname,
-    v_dnumber,
-    'DEPT',
-    'DEPT',
-    v_num_emps,
-    v_emp_totals.tot_hours,
-    v_emp_totals.tot_cost,
-    'HBROW',
-     v_insert_number);
-END report_ug;
+    for locrec in dname loop
+
+        v_dname.dname := locrec.dname;
+
+        open dnumber;
+        fetch dnumber into v_dnumber;
+        close dnumber;
+
+        open num_emps;
+        fetch num_emps into v_num_emps;
+        close num_emps;
+
+        open emp_totals;
+        fetch emp_totals into v_emp_totals;
+        close emp_totals;
+
+
+        cs450.ins_dept_summary(
+        v_dname,
+        v_dnumber,
+        'DEPT',
+        'DEPT',
+        v_num_emps,
+        v_emp_totals.tot_hours,
+        v_emp_totals.tot_cost,
+        'HBROW',
+        v_insert_number);
+    end loop;
+END;
 /
