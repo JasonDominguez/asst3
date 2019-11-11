329a948b5e4d6bf2a9ebb8c27f4c4a7992ee121a
hbrow <hbrow006@odu.edu>
2019-11-10 22:44:33 -0500

    :bug fixes

--- submit_files/report_ug.sql
+++ submit_files/report_ug.sql
@@ -1,36 +1,11 @@
--- this one is for the project_location report
--- This is an example problem to show what is required in the PL/SQL problem.
--- 
--- The problem is to produce a report on each project location, that is,
--- any location where any department has a project.  There is to be only
--- one report for each location, even if there are several projects there.
--- 
--- We want to know for each location:
--- 
--- lname: The name of that location;
--- num_proj: The number of projects in that location;
--- num_emp: The number of employees with addresses in that location;
--- num_dept: The number of departments with offices in that location;
--- num_work: The number of different employees who work on projects in that location;
--- tot_hours: The total hours worked by employees on projects in that location;
--- tot_cost: The total cost of those hours;
-
 CREATE OR REPLACE PROCEDURE report_ug AS
-   -- cursor to fetch the project locations
-   -- and the number of emps in each.
    CURSOR dep_name IS
    select distinct dname
    from department
 
    c_dep_name  dep_name%rowtype;
-   -- v_lname proj_loc.lname%type would seem more natural and useful
-   -- but it is not legal PL/SQL.
-   -- this record will hold the location :  c_proj_loc.lname 
-   -- that all the following cursors reference.
-   -- the cursor above, proj_loc, is the outer cursor.
-   -- the following cursors are opened once for each location.
 
-   -- the number of projects at the location
+
    CURSOR dep_num IS
    select distinct dnumber
    from department
@@ -38,8 +13,7 @@ CREATE OR REPLACE PROCEDURE report_ug AS
 
    c_dep_num dep_num%rowtype;
 
-   -- number of employees who work on projects
-   -- in this location
+
    CURSOR e_num IS
    select count(distinct ssn) e_num
    from (employee join works_on on ssn=essn) 
@@ -48,7 +22,7 @@ CREATE OR REPLACE PROCEDURE report_ug AS
 
    c_e_num e_num%rowtype;
 
-   -- total hours and cost of those employees
+
    CURSOR emp_totals IS
    select nvl(sum(hours),0) tot_hours, nvl(sum(hours*salary/2000),0) tot_cost
    from project left join (works_on join employee on essn=ssn) on pnumber = pno
@@ -63,8 +37,7 @@ BEGIN
    for locrec in dep_name loop
       c_dep_name.dname := locrec.dname;
 
-      -- all the inner cursors calculate single tuples
-      -- so no looping is necessary: just open, one fetch and close
+
       open dep_num;
       fetch dep_num into c_dep_num;
       close dep_num;
@@ -92,6 +65,6 @@ BEGIN
             i_num
             );
 
-   end loop; -- proj_loc
+   end loop; 
 END;
 /
