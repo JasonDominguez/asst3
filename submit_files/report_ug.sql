3413b8b1871585f24c407f3db49ec98c4b91aeb2
hbrow <hbrow006@odu.edu>
2019-11-10 20:15:13 -0500

    finished?

--- submit_files/report_ug.sql
+++ submit_files/report_ug.sql
@@ -2,62 +2,66 @@ CREATE OR REPLACE PROCEDURE ins_dept_summary(
        
        CURSOR dep_info IS
        select distinct dname dname, dnumber dnumber
-       from department
+       from department;
 
        p_dep_info dept_summary.dep_info%TYPE, 
        
        
-       
        CURSOR emp_type IS
-       
+       select ssn as 'DEPT'
+       from employee, department, project, works_on
+       where department.dname = 
+       and department.dnumber = employee.dno
+       and employee.ssn = works_on.essn
+       and works_on.pno = project.pnumber
+       and project.dnum = department.dnumber
+       group by ssn;
 
        p_emp_type dept_summary.emp_type%TYPE, 
-
-       
+    
 
        CURSOR proj_type IS
+       select ssn as 'DEPT'
+       from EMPLOYEE, DEPARTMENT, PROJECT, WORKS_ON
+       where department.dname = '$dep'
+       and department.dnumber = employee.dno
+       and employee.ssn = works_on.essn
+       and works_on.pno = project.pnumber
+       and project.dnum = department.dnumber
+       group by ssn
 
        p_proj_type dept_summary.proj_type%TYPE, 
        
-      
 
        CURSOR num_emps IS
        select count(distinct ssn) num_emps
        from (employee join department on dno=dnumber) 
-       where dname = c_dep_info.dname;
+       where dname = p_dep_info.dname;
 
        p_num_emps dept_summary.num_emps%TYPE, 
-       
        
 
        CURSOR emp_totals IS
        select nvl(sum(hours),0) tot_hours, nvl(sum(hours*salary/2000),0) tot_cost
        from project left join (works_on join employee on essn=ssn) on pnumber = pno
-       where plocation = c_proj_loc.lname;
+       where dname = p_dep_info.dname;
   
        p_emp_totals dept_summary.emp_totals%TYPE, 
-       
-       
-
-       CURSOR cost IS
-       
-       p_cost dept_summary.cost%TYPE,  
-       
-       
+        
 
        
-       p_insert_number NUMBER := 0;
+       p_insert_number NUMBER := 0);
 
 BEGIN
 
-    v_insert_number := v_insert_number + 1;
+    p_insert_number := p_insert_number + 1;
 
     cs450.ins_dept_summary(
        p_dep_info.dname ,
        p_dep_info.dnumber ,
-       v_emp_type ,
-       v_proj_type ,
-       v_num_emps ,
+       p_emp_type.emp_type ,
+       p_proj_type.proj_type ,
+       p_num_emps.num_emps ,
        p_emp_totals.tot_hours ,
        p_emp_totals.tot_cost ,
        'HBROW',
